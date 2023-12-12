d55877a9be04e520879952a25f9a6f1222ecb3b5
cal woodford <callum@woodford.info>
2023-12-12 15:57:28 +0000

    added: index.html info

--- scribbler.js
+++ scribbler.js
@@ -12,15 +12,19 @@ var getAll = function (selector, scope) {
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `scribbler
-            [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
+  var txt = `brum.ai
 
-            ###todo for new year dinner party
+            Welcome to the BrumAI docs.
 
-            - milk
-            - butter
-            - green onion
-            - lots and lots of kiwis 🥝`;
+            BrumAI is the home of AI in the Midlands.
+
+            We are a dynamic community dedicated to fostering innovation, collaboration, and learning in the field of artificial intelligence.
+
+            Our mission is to unite individuals passionate about AI, creating a platform where ideas are exchanged, knowledge is enriched, and connections are formed.
+            
+            Site and documentation created by David Mason, Callum Woodford, Taimoor Balal, Samantha German, Alexander Brown and Katarzyna Baginska.
+            `;
+
   var speed = 60;
 
   function typeItOut () {
